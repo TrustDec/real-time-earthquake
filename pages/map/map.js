32cf5acc57683cb97fdfb1d1cc9fdbe616a53eee
TrustTheBoy <603049583@qq.com>
2018-02-16 19:26:53 +0800

    no message

--- pages/map/map.js
+++ pages/map/map.js
@@ -9,6 +9,7 @@ Page({
     this.setData({
       longitude: option.longitude, 
       latitude: option.latitude, 
+      place: option.place,
       markers: [
         {
           id: 0, 
@@ -21,5 +22,21 @@ Page({
         }
       ]
     })
+  },
+  onShareAppMessage: function (res) {
+    if (res.from === 'button') {
+      // 来自页面内转发按钮
+      console.log(res.target)
+    }
+    return {
+      title: `${this.state.place}发生地震`,
+      path: `pages/map/map?place=${this.state.place}&longitude=${this.state.longitude}&latitude=${this.state.latitude}`,
+      success: function (res) {
+        // 转发成功
+      },
+      fail: function (res) {
+        // 转发失败
+      }
+    }
   }
 });
